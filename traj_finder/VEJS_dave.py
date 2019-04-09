c699299b8b99c58303e4107b440cfa9c8cfc282b
David Mueller <drmuelr@mit.edu>
2019-04-09 08:09:12 -0400

    remove lots of unused code

--- traj_finder/VEJS_dave.py
+++ traj_finder/VEJS_dave.py
@@ -3,26 +3,16 @@
 
 # In[1]:
 
-import numpy as np
-
 import astropy.units as u
 from astropy.time import Time
 from astropy.coordinates import solar_system_ephemeris
 
 from poliastro.bodies import Sun, Venus, Earth, Jupiter, Saturn
-from poliastro.twobody import Orbit
-from poliastro.maneuver import Maneuver
-from poliastro.iod import izzo
 from poliastro.plotting import OrbitPlotter2D
-from poliastro.util import norm
-import poliastro.twobody.propagation as Propagation
 
 from scipy import optimize as opt
 from node.transfer import TransferPlanner
 from node.flyby import Boundary, Flyby
-from poliastro.plotting.static import StaticOrbitPlotter
-from poliastro.util import norm
-from pexpect.screen import constrain
  
 solar_system_ephemeris.set("jpl")
 
@@ -48,17 +38,12 @@ def trajectory_calculator(t=None,plot_on=0,disp_on=False):
     time_e = t[1]
     time_j = t[2]
     time_s = t[3]
-    
-    time_ve = time_e-time_v
-    time_ej = time_j-time_e
-    time_js = time_s-time_j
-    
+
     route = [Venus, Earth, Jupiter, Saturn]
     
     lower_bound = 0
     upper_bound = 40*365
     
-    bounds = ((lower_bound, upper_bound),)*len(route)
     bounds = [(-7496.0,-90), (-7466.0,-60), (-7436.0,-30), (-7406.0,0)]
     options = {'maxiter': 100,'disp': True}
     
@@ -126,67 +111,20 @@ def trajectory_calculator(t=None,plot_on=0,disp_on=False):
                            constraints=constraint_list)
     print("opt_sol: ", opt_sol)
     
-    
-    #date_arrive_saturn = Time("2020-01-01", scale=TIME_SCALE)+time_s*u.year;
-#     date_arrive_saturn = time_s
-    
-#     planner = TransferPlanner()
-    ################## Jupiter ################## 
-#     planner.end_body = Saturn
-#     planner.start_body = Jupiter
-
-#     js_xfer = planner.make_transfer(end_epoch=date_arrive_saturn, 
-#                                     duration=time_js)
     js_xfer = xfer_list[2]
-# 
-#     #js_xfer.plot()
-#     if disp_on:
-#         print('computed J-S')
-    ################## Earth ################## 
-#     planner.end_body = Jupiter
-#     planner.start_body = Earth
-#     
-#     ej_xfer = planner.make_transfer(end_epoch=js_xfer.start_epoch, 
-#                                     duration=time_ej)
     ej_xfer = xfer_list[1]
+    ve_xfer = xfer_list[0]              
 
-#     #ej_xfer.plot()
-#     if disp_on:
-#         print('computed E-J')                            
-
-    ################## Venus2 ##################
-#     planner.end_body = Earth
-#     planner.start_body = Venus
-#     
-#     ve_xfer = planner.make_transfer(end_epoch=ej_xfer.start_epoch, 
-#                                     duration=time_ve)
-    ve_xfer = xfer_list[0]
-#     
-#     flyby_e = Flyby.from_transfers(ej_xfer, js_xfer)
-#     print("delta_v err at E: ", flyby_e.v_err)
-#     
-#     #ve_xfer.plot()
-#     if disp_on:
-#         print('computed V-E')                         
-
-    ################## Plot ##################  
-    
     xfers = {(Venus, Earth):    ve_xfer,
              (Earth, Jupiter):  ej_xfer,
              (Jupiter, Saturn): js_xfer}
-    
-    '''
-    orbits = {Venus:   ve_xfer.initial_orbit,
-              Earth:   ej_xfer.initial_orbit,
-              Jupiter: js_xfer.initial_orbit,
-              Saturn:  js_xfer.target_orbit }
-    '''
     
     encounters = {Venus:   Boundary.from_transfers(None, ve_xfer),
                   Earth:   flyby_list[0],
                   Jupiter: flyby_list[1],
                   Saturn:  Boundary.from_transfers(js_xfer, None)}
     
+    ################## Plot ##################  
     if plot_on:
         op = OrbitPlotter2D()
         
@@ -203,16 +141,6 @@ def trajectory_calculator(t=None,plot_on=0,disp_on=False):
         op.plot(ve_xfer.orbit, label="V2->E")
         op.plot(ej_xfer.orbit, label="E->J")
         op.plot(js_xfer.orbit, label="J->S")
-    
-    '''
-    orbits = (o_v2,o_e2,o_j,o_sf)
-    trajectories = (trx_v2e,trx_ej,trx_js)
-    delta_vs = (delv_e,delv_j,delv_s)
-    times = (date_flyby_venus2,date_flyby_earth,date_flyby_jupiter,date_arrive_saturn)
-    return (total_deltav,orbits,trajectories,delta_vs,times)
-    '''
-    #orbits = tuple(orbits.values())
-    #return (total_deltav,orbits,trajectories,delta_vs,times)
     
     print(tuple((k,v) for k,v in encounters.items()))
     orbits = tuple(v.parent_orbit for v in encounters.values())
@@ -254,60 +182,9 @@ def print_times(times):
     print("V-E: ", (times[1]-times[0]).to(u.year))
     print("E-J: ", (times[2]-times[1]).to(u.year))
     print("J-S: ", (times[3]-times[2]).to(u.year))
-        
-def total_deltav_calculator(tmod):
-    #print(tmod)
-    sol=trajectory_calculator(tmod,plot_on=0,disp_on=False)
-    #print(sol[0])
-    return sol[0].to(KPS).value
 
 def calc_max_deltav(v_inf,qratio=10, body=Earth):
     return 2*v_inf*(1+((v_inf)**2*(qratio*body.R)/(body.k)).to(u.m/u.m))**-1
-
-def weighted_deltav_calculator(tmod):
-        #print(tmod)
-        sol = trajectory_calculator(tmod, plot_on=0, disp_on=False)
-        #print(sol[3])
-        
-        # 10 is a weight
-        # 10*V_inf at Saturn + sum of all other deltaVs (including saturn)
-        return 10*sol[3][2].to(KPS).value + sum(sol[3]).to(KPS).value
-
-def saturn_deltav_calculator(tmod):
-    #print(tmod)
-    sol=trajectory_calculator(tmod,plot_on=0,disp_on=False)
-    #print(sol[3])
-    
-    
-    
-    return 5*sol[3][2].to(KPS).value + sum(sol[3]).to(KPS).value
-
-
-def earthflybyConstraint(ts):
-    
-    sol = trajectory_calculator(t=ts,plot_on=0,disp_on=False)
-    
-    deltavearth = sol[3][0].to(KPS)
-    v_earth = sol[1][1].v.to(KPS)
-    v_sat_leaving_earth = sol[2][1].v
-    v_inf = norm(v_sat_leaving_earth-v_earth).to(KPS)
-    max_deltav = calc_max_deltav(v_inf,qratio=2, body=Earth)
-    
-    return float((max_deltav-deltavearth)/max_deltav)
-
-def jupiter_flyby_constraint(ts):
-    body = Jupiter
-    q_ratio = 2
-        
-    sol = trajectory_calculator(t=ts, plot_on=0, disp_on=False)
-    
-    deltavjup = sol[3][1].to(KPS)
-    v_jup = sol[1][2].v.to(KPS)
-    v_sat_leaving_jup = sol[2][2].v
-    v_inf = norm(v_sat_leaving_jup-v_jup).to(KPS)
-    max_deltav = calc_max_deltav(v_inf,qratio=q_ratio, body=body)
-    
-    return float((max_deltav-deltavjup)/max_deltav)
 
 if __name__ == "__main__":
     
@@ -343,18 +220,14 @@ if __name__ == "__main__":
     trial_jupiter = Time("2039-02-26 12:23:01.441", scale=TIME_SCALE)
     trial_saturn =  Time("2046-02-26 06:23:01.441", scale=TIME_SCALE)
     
-    trial = [
-        trial_venus2,
-        trial_earth2,
-        trial_jupiter,
-        trial_saturn]
-    
-    #print("t_guess_cassini: ", t_guess_cassini)
+    trial = [trial_venus2,
+             trial_earth2,
+             trial_jupiter,
+             trial_saturn]
     
     #sol = trajectory_calculator(t_guess_cassini)
     sol = trajectory_calculator(t_guess_cassini2)
-    
-    #print("Tmod: ",opt_sol.x)
+
     print()
     print("Total deltav: ", sol[0])
     print("Delta vs: ",[x.value for x in sol[3]])
@@ -367,16 +240,13 @@ if __name__ == "__main__":
     t_ej=sol[4][2]-sol[4][1]
     t_js=sol[4][3]-sol[4][2]
     
-    
     trx_ve = sol[2][0]
     trx_ej = sol[2][1]
     trx_js = sol[2][2]
     
-    
     trx_ve_half = trx_ve.propagate(t_ve/2)
     trx_ej_half = trx_ej.propagate(t_ej/2)
     trx_js_half = trx_js.propagate(t_js/2)
-    
     
     op=OrbitPlotter2D()
     op.plot(sol[1][0],label='V')
